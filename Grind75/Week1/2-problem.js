a5edf7c300d9dd8d9587893f61626fd9b257be11
David Sánchez <deividsanchez96@hotmail.com>
2022-08-24 00:01:56 -0500

    Add problem 2

---
+++ Grind75/Week1/2-problem.js
@@ -0,0 +1,47 @@
+/**
+ *
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ *
+ * Input: s = "()"
+ * Output: true
+ *
+ * Input: s = "()[]{}"
+ * Output: true
+ *
+ * Input: s = "(]"
+ * Output: false
+ *
+ */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  const stack = [];
+
+  const map = new Map();
+  map.set("(", ")");
+  map.set("[", "]");
+  map.set("{", "}");
+
+  for (let i = 0; i < s.length; i += 1) {
+    if (map.has(s[i])) {
+      stack.push(map.get(s[i]));
+    } else if (s[i] !== stack.pop()) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};
+
+console.log(`is valid: ${isValid("()")}`);
+console.log(`is valid: ${isValid("()[]{}")}`);
+console.log(`is valid: ${isValid("(]")}`);
+console.log(`is valid: ${isValid("{[]}")}`);
